6122971466cb201389704af67ffe024dc8c7dc77
cassiusbessa <cassiusbessa@gmail.com>
2022-07-22 20:22:03 -0300

    tentando upar a docs

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const cliente = require('./Routes/cliente.routes');
 const ErrorHandler = require('../src/Middleware/ErrorHandler');
 
 const app = express();
-app.use(express.json());
 app.use('/', express.static('./view'));
+app.use(express.json());
 app.use('/admin', admin);
 app.use('/cliente', cliente);
 app.use(ErrorHandler);
