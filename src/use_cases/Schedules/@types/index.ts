94a790beeac2805199b475b9eee1fb0490ca5cda
cassiusbessa <cassiusbessa@gmail.com>
2022-11-06 00:44:33 -0300

    feat(utils): :art: implements schedules dto and serialize to model prisma

--- src/use_cases/Schedules/@types/index.ts
+++ src/use_cases/Schedules/@types/index.ts
@@ -1,6 +1,7 @@
 import { Prisma, Schedules } from '@prisma/client';
+import { ISchedule } from '../../../DTO/ISchedule';
 import IUseCases from '../../../interfaces/useCases/use_cases.interface';
 
 export type PrismaSchedulesMethods = Prisma.SchedulesDelegate<Prisma.RejectOnNotFound | Prisma.RejectPerOperation>;
 
-export type ISchedulesUseCases = IUseCases<Prisma.SchedulesCreateInput, Prisma.SchedulesWhereUniqueInput, Prisma.SchedulesWhereInput, Schedules, Prisma.SchedulesInclude>;
+export type ISchedulesUseCases = IUseCases<ISchedule, Prisma.SchedulesWhereUniqueInput, Prisma.SchedulesWhereInput, Schedules, Prisma.SchedulesInclude>;
---
+++ src/utils/adapter/prisma/serialize.ts
@@ -0,0 +1,48 @@
+import { Prisma } from '@prisma/client';
+import { ISchedule } from '../../../DTO/ISchedule';
+
+export default class PrismaScheduleAdapter {
+  public serialize = (data: ISchedule): Prisma.SchedulesCreateInput => {
+    const schedule = {
+      price: data.price,
+      date: data.date,
+      finished: data.finished,
+      client: {
+        connectOrCreate: {
+          where: {
+            phone: data.phone,
+          },
+          create: {
+            name: data.name,
+            phone: data.phone,
+            address_number: data.address_number,
+            address_complement: data.address_complement,
+            address: {
+              connectOrCreate: {
+                where: {
+                  cep: data.cep,
+                },
+                create: {
+                  cep: data.cep,
+                  street: data.street,
+                  city: data.city,
+                },
+              },
+            },
+          },
+        },
+      },
+      service: {
+        connect: {
+          id: data.serviceId,
+        },
+      },
+    };
+    return schedule;
+  };
+
+  public static deserialize = (data: any) => {
+    const deserialized = JSON.parse(data);
+    return deserialized;
+  };
+}
